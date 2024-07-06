45ab74aa8c2bb3f70ae8acaf430daf64781e23bd
Ellen Liu <allenliu789@gmail.com>
2024-07-05 23:10:04 -0700

    Add logic for error checking round 1
    * Add ids for input elements
    * Add styles for error visuals
    * Add logic to check day input for errors

--- js/logic.js
+++ js/logic.js
@@ -1,18 +1,64 @@
 //TODO: Check valid dates
 //TODO: Error when submitting incomplete input
-//TODO: Set text to red when error
+//TODO: Add logic to actually calculate date difference
+
 var buttonEl = document.getElementById("clickMe");
 if (buttonEl.addEventListener)
    buttonEl.addEventListener("click", calculateAge, false);
 else if (buttonEl.attachEvent)
    buttonEl.attachEvent('onclick', calculateAge);
 
+
 function calculateAge () {
-   var dayInput = document.getElementById("dayInput").value;
-   var monthInput = document.getElementById("monthInput").value;
-   var yearInput = document.getElementById("yearInput").value;
-   // console.log("Day input: " + dayInput);
-   // console.log("Month input: " + monthInput);
-   // console.log("Year input: " + yearInput);
-   
+   var dayInput = document.getElementById("dayInput");
+   var monthInput = document.getElementById("monthInput");
+   var yearInput = document.getElementById("yearInput");
+
+   var dayLabel = document.getElementById("dayLabel");
+   var monthLabel = document.getElementById("monthLabel");
+   var yearLabel = document.getElementById("yearLabel");
+
+   var dayValue = parseInt(dayInput.value);
+   var monthValue = parseInt(monthInput.value);
+   var yearValue = parseInt(yearInput.value);
+
+   console.log("Day input: " + dayValue);
+   console.log("Month input: " + monthValue);
+   console.log("Year input: " + yearValue);
+
+   var dayErrorEl = document.getElementById("dayError");
+   var monthErrorEl = document.getElementById("monthError");
+   var yearErrorEl = document.getElementById("yearError");
+
+   dayErrorEl.innerText = "";
+   monthErrorEl.innerText = "";
+   yearErrorEl.innerText = "";
+
+   dayInput.classList.remove("red-border");
+   monthInput.classList.remove("red-border");
+   yearInput.classList.remove("red-border");
+
+   dayLabel.classList.remove("errorFontRed");
+   monthLabel.classList.remove("errorFontRed");
+   yearLabel.classList.remove("errorFontRed");
+
+   //Check valid input
+   if (!dayValue){
+      dayErrorEl.innerText = "Must be a valid day";
+      dayInput.classList.add("red-border");
+      dayLabel.classList.add("errorFontRed");
+   }
+   if (!monthValue){
+      monthErrorEl.innerText = "Must be a valid month";
+      monthInput.classList.add("red-border");
+      monthLabel.classList.add("errorFontRed");
+   }
+   if (!yearValue){
+      yearErrorEl.innerText = "Must be in the past";
+      yearInput.classList.add("red-border");
+      yearLabel.classList.add("errorFontRed");
+   }
+   if (!dayValue || !monthValue || !yearValue) {
+      return;
+   }
 }
